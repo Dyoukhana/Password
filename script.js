07c1d060101d7a3ca528b106f563d005a1815ad1
Donovan Youkhana <dvonabil@gmail.com>
2019-10-17 21:39:25 -0700

    Added alerts to my password generator

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-//random password
-function generate(){
+document.getElementById("password-btn").addEventListener("click", function() {
+  getPasswordOptions();
+});
 
-    //set password length/complexity
-    let complexity = document.getElementById("slider").value;
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  );
 
-    //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+  if (isNaN(length)) {
+    alert("Password length must be a number");
+    return;
+  }
 
-    let password = "";
+  if (length < 8) {
+    alert("Password length must be greater than 8");
+    return;
+  }
 
-    //create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
+  if (length > 128) {
+    alert("Password length must be less than 128");
+    return;
+  }
 
-    //add password to textbox/display area
-    document.getElementById("display").value = password;
+  var specialChars = confirm("Would you like to use special character?");
 
-    //add password to textbox
-    document.getElementById("display").value = password;
+  var numericChars = confirm("Would you like to use numeric character?");
 
+  var uppercaseChars = confirm("Would you like to use uppercase character?");
+
+  var lowercaseChars = confirm("Would you like to use lowercase character?");
+
+  if (!specialChars && !numericChars && !uppercaseChars && !lowercaseChars) {
+    alert("We can't help you, you are to picky...");
+    return;
+  }
+
+  var passwordOptions = {
+    specialChars: specialChars,
+    numericChars: numericChars,
+    uppercaseChars: uppercaseChars,
+    lowercaseChars: lowercaseChars
+  };
+  // var passwordOptions = {
+  //   specialChars,
+  //   numericChars,
+  //   uppercaseChars,
+  //   lowercaseChars
+  // };
+
+  return passwordOptions;
 }
 
+    
+        
+      
+    
+    
+
+
+
+  
